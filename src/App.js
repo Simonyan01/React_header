a33adc6d8707fb90c0681c06ab09a56f70ec1063
Simonyan01 <lyova.simonyan.01@mail.ru>
2022-08-18 21:12:20 +0400

    Changed

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+function App() {
+  return (
+    <body>
+      <header>
+        <div className="header ">
+          <div className="container ">
+            <div className="header_inner ">
+              <div className="header_Logo ">
+                <a className=" Logo " href="# ">Օնլայն Հարթակ</a>
+              </div>
+              <nav className="nav ">
+                <a className="navlink " href="# ">About</a>
+                <a className="navlink " href="# ">Service</a>
+                <a className="navlink " href="# ">Blog</a>
+                <a className="navlink " href="# ">My Works</a>
+                <a className="navlink " href="# ">Contact</a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+    </body>
+  );
+}
+
+export default App;
